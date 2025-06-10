ef997b0bfb58d69823ceefd57fa10c52b9e2e194
gvia <gritcaev.vv@gmail.com>
2025-06-10 16:39:40 +0300

    Добавлены модели Product и Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Модель категории товара.
+    """
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(blank=True, verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """
+    Модель продукта (товара) в каталоге.
+    """
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', null=True, blank=True, verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return self.name
