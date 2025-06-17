2b366249bb5b7407d8e65acffd33904c331c8643
gvia <gritcaev.vv@gmail.com>
2025-06-18 00:59:51 +0300

    реализован постраничный вывод товаров на главной с навигацией

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Product
 
 
 def home(request):
-    products = Product.objects.all()
-    return render(request, 'catalog/home.html', {'products': products})
+    product_list = Product.objects.all()
+    paginator = Paginator(product_list, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'catalog/home.html', {'page_obj': page_obj})
 
 
 def contacts(request):
