1011d489c61171c35f3c5fa7f9e03986b6ff75a9
gvia <gritcaev.vv@gmail.com>
2025-06-18 14:45:11 +0300

    Создано приложение blog и модель Post с нужными полями

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержание')
+    preview = models.ImageField(upload_to='blog_preview/', blank=True, null=True, verbose_name='Превью')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='Публиковать')
+    views = models.PositiveIntegerField(default=0, verbose_name='Просмотры')
+
+    class Meta:
+        verbose_name = 'Блоговая запись'
+        verbose_name_plural = 'Блоговые записи'
+
+    def __str__(self):
+        return self.title
