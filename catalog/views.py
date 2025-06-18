ba9b2e620b0f9fc3c0a1e4c10e1e43112d03963d
gvia <gritcaev.vv@gmail.com>
2025-06-18 13:59:47 +0300

    Перевод контроллеров catalog на CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,47 +1,57 @@
 from django.contrib import messages
+from django.views import View
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
+from django.views.generic import ListView, TemplateView, DetailView
 from .models import Product, ContactInfo, Category
 
 
-def catalog_view(request):
-    categories = Category.objects.all()
-    return render(request, 'catalog/catalog.html', {'categories': categories})
+class HomeView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'page_obj'
+    paginate_by = 6
+    ordering = ['-created_at']
 
 
-def category_products(request, category_id):
-    category = get_object_or_404(Category, id=category_id)
-    products = category.products.order_by('-created_at')
-    return render(request, 'catalog/category_products.html', {
-        'category': category,
-        'products': products
-    })
+class ContactView(TemplateView):
+    template_name = 'catalog/contacts.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['contact'] = ContactInfo.objects.first()
+        return context
 
-def home(request):
-    product_list = Product.objects.order_by('-created_at')
-    paginator = Paginator(product_list, 6)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'catalog/home.html', {'page_obj': page_obj})
-
-
-def contacts(request):
-    contact = ContactInfo.objects.first()
-
-    if request.method == 'POST':
-        name = request.POST.get('name')
+    def post(self, request, *args, **kwargs):
+        name =request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
-
 
         # Здесь можно сохранить сообщение в БД или отправить на почту
 
         messages.success(request, f'Спасибо, {name}, ваше сообщение отправлено!')
-        return redirect('contacts')
+        return self.get(request, *args, **kwargs)
 
-    return render(request, 'catalog/contacts.html', {'contact': contact})
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'catalog/product_detail.html', {'product': product})
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+
+
+class CatalogView(View):
+
+    def get(self, request):
+        categories = Category.objects.all()
+        return render(request, 'catalog/catalog.html', {'categories': categories})
+
+
+class CategoryProductsView(View):
+
+    def get(self, request, category_id):
+        category = get_object_or_404(Category, id=category_id)
+        products = category.products.order_by('-created_at')
+        return render(request, 'catalog/category_products.html', {
+            'category': category,
+            'products': products
+        })
