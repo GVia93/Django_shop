eddf91ae03f858ada5643a35b4508b9e2367d40b
gvia <gritcaev.vv@gmail.com>
2025-07-07 17:34:47 +0300

    Исправлены ошибки, добалены docstring

--- users/forms.py
+++ users/forms.py
@@ -5,26 +5,57 @@ from users.models import CustomUser
 
 
 class CustomUserCreateForm(UserCreationForm):
+    """
+    Форма для регистрации нового пользователя.
+    """
+
     phone_number = forms.CharField(
         max_length=15,
         required=False,
         help_text="Необязательное поле. Введите ваш номер телефона.",
     )
+    country = forms.CharField(required=False)
+    avatar = forms.ImageField(required=False)
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ("username", "email", "phone_number", "password1", "password2")
+        fields = (
+            "username",
+            "email",
+            "country",
+            "phone_number",
+            "avatar",
+            "password1",
+            "password2",
+        )
 
     def clean_phone_number(self):
+        """
+        Проверяет, что номер телефона содержит только цифры, если он указан.
+        """
         phone_number = self.cleaned_data.get("phone_number")
         if phone_number and not phone_number.isdigit():
             raise forms.ValidationError("Номер телефона должен содержать только цифры.")
         return phone_number
 
+    def __init__(self, *args, **kwargs):
+        """
+        Добавляет Bootstrap-классы ко всем полям формы.
+        """
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
 
 class CustomAuthenticationForm(AuthenticationForm):
+    """
+    Форма для входа пользователя.
+    """
 
     def __init__(self, *args, **kwargs):
+        """
+        Добавляет Bootstrap-классы ко всем полям формы.
+        """
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs["class"] = "form-control"
--- users/views.py
+++ users/views.py
@@ -7,18 +7,29 @@ from .models import CustomUser
 
 
 class CustomRegisterView(CreateView):
+    """
+    Страница регистрации нового пользователя.
+    """
+
     model = CustomUser
     form_class = CustomUserCreateForm
-    template_name = "users:register"
-    success_url = reverse_lazy("catalog:home")
+    template_name = "users/register.html"
+    success_url = reverse_lazy("users:login")
 
 
 class CustomLoginView(LoginView):
+    """
+    Страница для входа пользователя в систему.
+    """
+
     template_name = "users/login.html"
     form_class = CustomAuthenticationForm
     success_url = reverse_lazy("catalog:home")
 
 
 class CustomLogoutView(LogoutView):
+    """
+    Страница для выхода пользователя из системы.
+    """
 
-    next_page = reverse_lazy("users:login")
+    template_name = "users/logout.html"
